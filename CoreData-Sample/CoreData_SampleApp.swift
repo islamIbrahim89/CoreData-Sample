d8899edace82697b5fee299494b9a8ef036966dc
Islam Moussa <mousai@aecl.com>
2025-12-10 16:13:12 +0300

    init commit

---
+++ CoreData-Sample/CoreData_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreData_SampleApp.swift
+//  CoreData-Sample
+//
+//  Created by islam moussa on 10/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct CoreData_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
